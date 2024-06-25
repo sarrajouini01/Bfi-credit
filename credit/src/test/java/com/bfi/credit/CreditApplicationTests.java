086e10be41da9c1bfd7d7ced9285944911f7bb51
sarrajouini01 <sarra.jouini@esprit.tn>
2024-06-25 11:47:02 +0100

    Initial commit

---
+++ credit/src/test/java/com/bfi/credit/CreditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bfi.credit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
